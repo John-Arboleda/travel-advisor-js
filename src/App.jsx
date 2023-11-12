231b713f8b365e1f7152ced1bf815d4649b4725f
John-Arboleda <johnjack0126@gmail.com>
2023-11-12 09:25:12 -0500

    Set up dependencies and folder struc

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { CssBaseline, Grid } from '@mui/material';
+// import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+import PlaceDetails from './components/PlaceDetails/PlaceDetails';
 
+
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} styles={{ width: '100%'}}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
     </>
+    
+      
+    
   )
 }
 
